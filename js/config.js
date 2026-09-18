ada9de3452df07a15da3986d6642f6231d01f709
shoislom <shoislommm@bk.ru>
2026-09-18 18:11:37 +0500

    addedimage of speaker and ids of bitrix forms

--- js/config.js
+++ js/config.js
@@ -15,17 +15,17 @@
   /* Входящий вебхук портала с правом crm.
      Пример: "https://teamuni.bitrix24.ru/rest/12/xxxxxxxxxxxxxxxx/crm.lead.add.json"
      Пока строка пустая — форма работает в демо-режиме и лид не создаётся. */
-  var BITRIX24_WEBHOOK_URL = "";
+  var BITRIX24_WEBHOOK_URL =
+    "https://teamuni.bitrix24.ru/rest/129/7o3qxas6wu7bvxsr/crm.lead.add.json";
 
   /* Источник лида в CRM: WEB / ADVERTISING / CALL / собственный код справочника. */
-  var BITRIX_SOURCE_ID = "WEB";
+  var BITRIX_SOURCE_ID = "34";
 
   /* Пользовательские поля лида. Пока в константе есть 'XXXX',
      поле не отправляется — как в лендинге вступительных экзаменов. */
-  var UF_ORGANIZATION   = "UF_CRM_1XXXXXXXXX";
-  var UF_ATTENDEE_TYPE  = "UF_CRM_2XXXXXXXXX";
-  var UF_QUESTION       = "UF_CRM_3XXXXXXXXX";
-  var UF_PAGE_LANGUAGE  = "UF_CRM_4XXXXXXXXX";
+  var UF_ORGANIZATION = "UF_CRM_YOURORGANISAT";
+  var UF_ATTENDEE_TYPE = "UF_CRM_DOLZHNOST";
+  var UF_QUESTION = "UF_CRM_1779434984532";
 
   /* Единые английские подписи для значений select — в CRM не должны
      попадать сырые коды вроде "entrepreneur". */
@@ -33,7 +33,7 @@
     student: "Student",
     entrepreneur: "Entrepreneur",
     professional: "Professional / manager",
-    other: "Other"
+    other: "Other",
   };
 
   /* Префикс заголовка лида: "TEAM Public Talks - <имя> (<кто>)" */
@@ -45,7 +45,8 @@
        Формат UTC: 28.09.2026, 17:30–21:00 по Ташкенту (UTC+5). */
     calendarStartUtc: "20260928T123000Z",
     calendarEndUtc: "20260928T160000Z",
-    mapUrl: "https://www.google.com/maps/search/?api=1&query=TEAM+University+Tashkent"
+    mapUrl:
+      "https://www.google.com/maps/search/?api=1&query=TEAM+University+Tashkent",
   };
 
   /* ------------------------------------------------------------- программа
@@ -54,11 +55,11 @@
      duration в минутах; 0 — блок без фиксированного окончания. */
   var AGENDA = [
     { id: "registration", time: "17:30", duration: 30 },
-    { id: "opening",      time: "18:00", duration: 10 },
-    { id: "lecture",      time: "18:10", duration: 60 },
-    { id: "coffee",       time: "19:10", duration: 15 },
-    { id: "qa",           time: "19:25", duration: 60 },
-    { id: "networking",   time: "20:25", duration: 0 }
+    { id: "opening", time: "18:00", duration: 10 },
+    { id: "lecture", time: "18:10", duration: 60 },
+    { id: "coffee", time: "19:10", duration: 15 },
+    { id: "qa", time: "19:25", duration: 60 },
+    { id: "networking", time: "20:25", duration: 0 },
   ];
 
   window.TPT_CONFIG = {
@@ -71,11 +72,11 @@
     UF_ORGANIZATION: UF_ORGANIZATION,
     UF_ATTENDEE_TYPE: UF_ATTENDEE_TYPE,
     UF_QUESTION: UF_QUESTION,
-    UF_PAGE_LANGUAGE: UF_PAGE_LANGUAGE,
+
     ATTENDEE_TYPE_LABELS: ATTENDEE_TYPE_LABELS,
     LEAD_TITLE_PREFIX: LEAD_TITLE_PREFIX,
 
     EVENT: EVENT,
-    AGENDA: AGENDA
+    AGENDA: AGENDA,
   };
 })(window);
